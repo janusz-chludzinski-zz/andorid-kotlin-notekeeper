75047a25fef3c1e689943a2abcecec9335a96edb
Gianni <chludzinski.janusz@gmail.com>
2020-01-16 08:10:38 +0100

    RecyclerAdapter

--- app/src/main/java/com/example/notekeeper/note_edit/NotePresenter.kt
+++ app/src/main/java/com/example/notekeeper/note_edit/NotePresenter.kt
@@ -4,27 +4,39 @@ import com.example.notekeeper.DataManager
 import com.example.notekeeper.NoteInfo
 import com.example.notekeeper.POSITION_NOT_SET
 
-class NotePresenter(private val activity: MainActivity) {
+class NotePresenter(private val activity: NoteActivity) {
 
     private var notePosition = POSITION_NOT_SET
 
     fun updateValues(newNoteInfo: NoteInfo) {
-        DataManager.notes[notePosition] = newNoteInfo
+        if (notePosition == POSITION_NOT_SET)
+            createNewNote(newNoteInfo)
+        else
+            DataManager.notes[notePosition] = newNoteInfo
+    }
+
+    private fun createNewNote(newNoteInfo: NoteInfo) {
+        DataManager.notes.add(newNoteInfo)
     }
 
     fun displayNote(notePosition: Int) {
-        if(notePosition != POSITION_NOT_SET) {
+        if (notePosition != POSITION_NOT_SET) {
             val note = DataManager.notes[notePosition]
 
-            activity.setCourseInfo(resolveCoursePosition(note))
-            activity.setNoteTitle(note.title)
-            activity.setNoteText(note.note)
+            setActivityData(note)
+
+            this.notePosition = notePosition
         }
     }
 
     fun displayNextNote() {
         resolveNotePosition()
         displayNote(notePosition)
+    }
+
+    fun provideCoursesList() {
+        val courses = DataManager.courses.values.toList()
+        activity.propagateData(courses)
     }
 
     private fun resolveNotePosition() {
@@ -38,6 +50,10 @@ class NotePresenter(private val activity: MainActivity) {
         return DataManager.courses.values.indexOf(note.course)
     }
 
-
+    private fun setActivityData(note: NoteInfo) {
+        activity.setCourseInfo(resolveCoursePosition(note))
+        activity.setNoteTitle(note.title)
+        activity.setNoteText(note.note)
+    }
 }
 
