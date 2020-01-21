3ff546db57c354a26acc008f5f98848e90000326
Gianni <chludzinski.janusz@gmail.com>
2020-01-21 13:34:30 +0100

    animation

--- app/src/main/java/com/example/notekeeper/note_list/NoteListActivity.kt
+++ app/src/main/java/com/example/notekeeper/note_list/NoteListActivity.kt
@@ -1,11 +1,14 @@
 package com.example.notekeeper.note_list
 
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.notekeeper.R
 import com.example.notekeeper.R.string.drawer_close
@@ -13,11 +16,15 @@ import com.example.notekeeper.R.string.drawer_open
 import com.example.notekeeper.note_edit.NoteActivity
 import kotlinx.android.synthetic.main.activity_note_list.*
 import kotlinx.android.synthetic.main.content_note_list.*
+import kotlinx.android.synthetic.main.drawer_header.*
+import kotlinx.android.synthetic.main.drawer_header.view.*
+
 
 class NoteListActivity : AppCompatActivity() {
 
     private val presenter = NoteListPresenter(this)
     lateinit var toggle: ActionBarDrawerToggle
+    lateinit var animationDrawable: AnimationDrawable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,12 +38,18 @@ class NoteListActivity : AppCompatActivity() {
         listItems.layoutManager = LinearLayoutManager(this)
         listItems.adapter = NoteRecyclerAdapter(this)
 
+        val view = layoutInflater.inflate(R.layout.drawer_header, navigationDrawer, true)
+        animationDrawable = view.menu_header.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(2000)
+        animationDrawable.setExitFadeDuration(4000)
+        animationDrawable.start()
+
         toggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(toggle.onOptionsItemSelected(item)) {
+        if (toggle.onOptionsItemSelected(item)) {
             return true
         }
         return super.onOptionsItemSelected(item)
@@ -46,6 +59,7 @@ class NoteListActivity : AppCompatActivity() {
         super.onResume()
         populateNoteList()
         manageToast()
+
     }
 
     private fun manageToast() {
